1e1129cd6af1276d81c6795233fa069ceeca99a5
akolesnikov <alex_maykop@yahoo.com>
2024-05-12 11:42:11 +0300

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.AllowContentType())
+	r.Use(middleware.RequestID)
+	r.Use(middleware.URLFormat)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	err := http.ListenAndServe(":80", r)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
